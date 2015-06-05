555d2db902071673f06820f9c5a239d0d77b5f0b
Kyrre Sjobak <k.n.sjobak@fys.uio.no>
2015-06-05 15:13:16 +0200

    New generation of analysis/plotting scripts for T3P: Actually readable and understandable code!

---
+++ t3p_ng/Wakefile.py
@@ -0,0 +1,129 @@
+#Classes for loading a wake file
+
+import numpy as np
+import scipy.integrate as sciInt
+
+class Wake:
+    s = None # [m]
+
+    V = None # [V/pC]
+    I = None # [C/m]
+
+    x = None # [m]
+    y = None # [m]
+
+    Q = None # [C]
+
+    def __init__(self,s,V,I,x,y):
+        self.s = np.asarray(s)
+        self.V = np.asarray(V)
+        self.I = np.asarray(I)
+        self.x = x
+        self.y = y
+        assert len(s) == len(V)
+        assert len(s) == len(I)
+        
+        self.Q = sciInt.simps(self.I, x=self.s)*1e12
+        print "Q = ", self.Q, "[C]"
+
+class ImpedanceSpectrum:
+    wake = None
+    
+    V_FFT = None
+    I_FFT = None
+    
+    Z = None
+
+    t = None
+    f = None
+    
+    cutIdx = None
+    goodIdx = None
+    
+    c = 299792458 #[m/s]
+    
+    def __init__(self,wake):
+        self.wake = wake
+        
+        self.t = self.wake.s/self.c
+
+        self.V_FFT = 1e12*self.t[-1]*np.fft.rfft(self.wake.V)/(len(self.wake.V)-1) #1e12 to scale pC^-1 -> C^-1
+        self.I_FFT = self.t[-1]*np.fft.rfft(self.wake.I)/(len(self.wake.I)-1)
+        
+        self.f = np.fft.fftfreq(self.t.size, d=self.t[1]-self.t[0])
+
+        if not self.f[len(self.V_FFT)] >= 0.0:
+            self.cutIdx = len(self.V_FFT)
+        else:
+            self.cutIdx = len(self.V_FFT)-1
+        self.f = self.f[:self.cutIdx]
+            
+        I0 = np.abs(self.I_FFT[0])
+        self.goodIdx = self.cutIdx
+        for i in xrange(self.cutIdx):
+            if np.abs(self.I_FFT[i])/I0 < np.exp(-6):
+                self.goodIdx = i
+                print "Cutting at f=%g [GHz] corresponding to I0*exp(-3), cutLen/cutIdx=%g %%, goodLen=%i" % (self.f[i]/1e9, 100*float(self.goodIdx)/float(self.cutIdx), self.goodIdx)
+                break
+
+        self.Z = self.V_FFT/(3e8*self.I_FFT)
+
+def loadWakeFile(fname):
+    wakes = []
+    
+    #loadStrings = ""
+    s = []
+    V = []
+    I = []
+    x = None
+    y = None
+    
+    wfile = open(fname,'r')
+    
+    inCommentBlock = True
+    commentBlockLineCounter = 0
+    for l in wfile:
+        if l[0] == '#':
+            if not inCommentBlock:
+                if commentBlockLineCounter < 0:
+                    wakes.append(Wake(s,V,I,x,y))
+                    s = []
+                    V = []
+                    I = []
+                    x = None
+                    y = None
+
+                inCommentBlock = True
+                commentBlockLineCounter = 0
+            
+            commentBlockLineCounter = commentBlockLineCounter + 1
+                
+            if commentBlockLineCounter == 2:
+                (x,y) = map(float,l[2:-2].split(','))
+                print "reading:",x,y
+        else:
+            if inCommentBlock:
+                assert commentBlockLineCounter == 4
+                inCommentBlock = False
+    
+            # Slow and ineff, but whatevs.
+            sVI = l.split()
+            s.append(float(sVI[0]))
+            V.append(float(sVI[1]))
+            I.append(float(sVI[2]))
+            
+    #last one
+    wakes.append(Wake(s,V,I,x,y))
+    s = []
+    V = []
+    I = []
+    x = None
+    y = None
+    wfile.close()
+    
+    return wakes
+
+
+# import sys
+# wakes = loadWakeFile(sys.argv[1])
+# print wakes
---
+++ t3p_ng/plotWake.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+from Wakefile import *
+
+print 'Usage:'
+print 'plotWake.py wakefile1(--name1) wakefile2(--name2) etc.'
+
+wakes = []
+imps  = []
+names = []
+
+for arg in sys.argv[1:]:
+    args = arg.split('--')
+    if len(args) == 1:
+        wakes.append( loadWakeFile(arg) )
+        names.append( arg )
+    elif len(args) == 2:
+        wakes.append( loadWakeFile(args[0]) )
+        names.append( args[1] )
+    else:
+        print "ERROR"
+        exit
+    imp = []
+    for w in wakes[-1]:
+        imp.append(ImpedanceSpectrum(w))
+    imps.append(imp)
+
+for (w,i,n) in zip(wakes,imps,names):
+    print "Plotting '"+n+"'"
+    plt.figure(1)
+    wl = plt.plot(w[0].s, w[0].V, label=n)
+    
+    wlc = wl[0].get_color()
+
+    plt.figure(2)
+    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.real(i[0].Z[:i[0].goodIdx]), color=wlc, ls='-', label=n)
+    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.imag(i[0].Z[:i[0].goodIdx]), color=wlc, ls='--')
+    
+    
+    plt.figure(3)
+    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.abs(i[0].Z[:i[0].goodIdx]), label=n)
+    #print i[0].f[:i[0].goodIdx]
+
+plt.figure(1)
+plt.legend()
+plt.xlabel("s [m]")
+plt.ylabel("V [V/pC]")
+
+plt.figure(2)
+plt.legend()
+plt.xlabel("f [GHz]")
+plt.ylabel("Z [$\Omega$]")
+
+
+plt.figure(3)
+plt.legend()
+plt.xlabel("f [GHz]")
+plt.ylabel("|Z| [$\Omega$]")
+
+plt.show()
