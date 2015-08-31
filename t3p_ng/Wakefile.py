Fixing units on impedance (Use Ohms rather than picoOhms) in t3p_ng; they were off due to copy-pasting from "old generation" scripts where the charge distribution was normalized with the total charge.
Also replace 3e8 -> c=2.99792458e8 m/s, and plot ZL/n where I expect the imaginary part to be constant below the first cutoff.

t3p_ng/Wakefile.py
@@ -6,13 +6,13 @@ import scipy.integrate as sciInt
 class Wake:
     s = None # [m]
 
-    V = None # [V/pC]
+    V = None # [V]
     I = None # [C/m]
 
     x = None # [m]
     y = None # [m]
 
-    Q = None # [C]
+    Q = None # [pC]
 
     def __init__(self,s,V,I,x,y):
         self.s = np.asarray(s)
@@ -45,10 +45,10 @@ class Wake:
 class ImpedanceSpectrum:
     wake = None
     
-    V_FFT = None
-    I_FFT = None
+    V_FFT = None # [V/frequency]
+    I_FFT = None # [A/frequency]
     
-    Z = None
+    Z = None # [Ohm]
 
     t = None
     f = None
@@ -63,8 +63,8 @@ class ImpedanceSpectrum:
         
         self.t = self.wake.s/self.c
 
-        self.V_FFT = 1e12*self.t[-1]*np.fft.rfft(self.wake.V)/(len(self.wake.V)-1) #1e12 to scale pC^-1 -> C^-1
-        self.I_FFT = self.t[-1]*np.fft.rfft(self.wake.I)/(len(self.wake.I)-1)
+        self.V_FFT = self.t[-1]*np.fft.rfft(self.wake.V)/(len(self.wake.V)-1)
+        self.I_FFT = self.c * self.t[-1]*np.fft.rfft(self.wake.I)/(len(self.wake.I)-1)
         
         self.f = np.fft.fftfreq(self.t.size, d=self.t[1]-self.t[0])
 
@@ -82,7 +82,7 @@ class ImpedanceSpectrum:
                 print "Cutting at f=%g [GHz] corresponding to I0*exp(-3), cutLen/cutIdx=%g %%, goodLen=%i" % (self.f[i]/1e9, 100*float(self.goodIdx)/float(self.cutIdx), self.goodIdx)
                 break
 
-        self.Z = self.V_FFT/(3e8*self.I_FFT)
+        self.Z = self.V_FFT/self.I_FFT
 
 class Envelope:
     wake = None

t3p_ng/plotWake.py
@@ -63,7 +63,7 @@ if len(wakes) == 0:
 for (w,i,e,n) in zip(wakes,imps,envs,names):
     print "Plotting '"+n+"'"
     plt.figure(1)
-    wl = plt.plot(w[0].s, w[0].V, label=n)
+    wl = plt.plot(w[0].s, w[0].V/w[0].Q, label=n)
     
     wlc = wl[0].get_color()
 
@@ -78,7 +78,12 @@ for (w,i,e,n) in zip(wakes,imps,envs,names):
     
     plt.figure(4)
     plt.semilogy(e[0].s,e[0].Venv,label=n)
-
+    
+    plt.figure(5)
+    freqRatioN = i[0].f[:i[0].goodIdx] / 11.2455e3 #f/f_rev
+    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.real(i[0].Z[:i[0].goodIdx]) / freqRatioN, color=wlc, ls='-', label=n)
+    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.imag(i[0].Z[:i[0].goodIdx] / freqRatioN), color=wlc, ls='--')
+    
 plt.figure(1)
 plt.legend()
 plt.xlabel("s [m]")
@@ -99,5 +104,10 @@ plt.figure(4)
 plt.legend()
 plt.xlabel("s [m]")
 plt.ylabel("V [V/pC]")
+
+plt.figure(5)
+plt.legend()
+plt.xlabel("f [GHz]")
+plt.ylabel("Z [$\Omega$]/($f/f_{rev}$)")
 
 plt.show()