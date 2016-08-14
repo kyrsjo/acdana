c372a16b4a5858bf17748e56c34f0eec144a4dec
Kyrre Sjobak <kyrsjo@gmail.com>
2016-08-14 18:14:35 +0200

    findBadBCs now works :)

--- mesh/build.sh
+++ mesh/build.sh
@@ -1,5 +1,5 @@
 #g++ SimpleXyRd.cpp `ncxx4-config  --libs` -Wall -o SimpleXyRd
 #g++ findBadBC.cpp `ncxx4-config  --libs` -Wall -o findBadBC
 
-gcc simple_xy_rd.c -Wall `nc-config --cflags --libs` -o simple_xy_rd
-gcc findBadBC.c -Wall `nc-config --cflags --libs` -o findBadBC
+#gcc simple_xy_rd.c -Wall `nc-config --cflags --libs` -o simple_xy_rd
+gcc findBadBC.c -Wall -g `nc-config --cflags --libs` -o findBadBC
--- mesh/findBadBC.c
+++ mesh/findBadBC.c
@@ -23,7 +23,26 @@ size_t lonely_tris_num;
 FILE* f2;
 size_t doubleBC;
 
-void checkTri(int idx1, int idx2, int idx3, int SS, double* coords, int inTet);
+void checkTri(int idx1, int idx2, int idx3, int SS, int inTet);
+int findInternal(int i1, int i2, int i3);
+
+//Dimensions
+size_t ncoords,
+  tetexterior,tetexteriorsize,
+  tetinterior,tetinteriorsize;
+
+//Data
+int tet_ext_id;
+nc_type tet_ext_type;
+int* tet_ext;
+
+int coords_id;
+//nc_type coords_type;
+double* coords;
+
+int tet_int_id;
+//nc_type tet_int_type;
+int* tet_int;
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -35,20 +54,11 @@ int main(int argc, char* argv[]) {
 
   /* This will be the netCDF ID for the file, dimensions, and data variables. */
   int ncid;
-  int ncoords_dim,tetexterior_dim,tetexteriorsize_dim;
+  int ncoords_dim,
+    tetexterior_dim,tetexteriorsize_dim,
+    tetinterior_dim, tetinteriorsize_dim;
 
-  //Dimensions
-  size_t ncoords,tetexterior,tetexteriorsize;
-
-  //Data
-  int tet_ext_id;
-  nc_type tet_ext_type;
-  int* tet_ext;
-
-  int coords_id;
-  //nc_type coords_type;
-  double* coords;
-  
+  // (the actual data is global)
   
   /* error handling. */
   int retval;
@@ -75,12 +85,33 @@ int main(int argc, char* argv[]) {
   if ((retval = nc_inq_dimlen(ncid, tetexteriorsize_dim, &tetexteriorsize)))
     ERR(retval);
   printf("tetexteriorsize=%zu\n",tetexteriorsize);
+  if (tetexteriorsize != 9) {
+    printf("Expected tetexteriorsize==\n9");
+    exit(1);
+  }
 
-  /* Get the varid of the data variable, based on its name. */
+  if ((retval = nc_inq_dimid(ncid, "tetinterior", &tetinterior_dim)))
+    ERR(retval);
+  if ((retval = nc_inq_dimlen(ncid, tetinterior_dim, &tetinterior)))
+    ERR(retval);
+  printf("tetinterior=%zu\n",tetinterior);
+  
+  if ((retval = nc_inq_dimid(ncid, "tetinteriorsize", &tetinteriorsize_dim)))
+    ERR(retval);
+  if ((retval = nc_inq_dimlen(ncid, tetinteriorsize_dim, &tetinteriorsize)))
+    ERR(retval);
+  printf("tetinteriorsize=%zu\n",tetinteriorsize);
+  if (tetinteriorsize != 5) {
+    printf("Expected tetinteriorsize==5\n");
+    exit(1);
+  }
+  
+  printf("Loading mesh data...");
+  
   if ((retval = nc_inq_varid(ncid, "tetrahedron_exterior", &tet_ext_id)))
     ERR(retval);
-  if ((retval = nc_inq_var(ncid, tet_ext_id, NULL, &tet_ext_type,NULL,NULL,NULL)))
-    ERR(retval);
+  //if ((retval = nc_inq_var(ncid, tet_ext_id, NULL, &tet_ext_type,NULL,NULL,NULL)))
+  //  ERR(retval);
   //TODO: Check that type is correct...
   tet_ext = malloc(sizeof(int)*tetexterior*tetexteriorsize);
   if ((retval = nc_get_var_int(ncid, tet_ext_id, tet_ext)))
@@ -88,10 +119,22 @@ int main(int argc, char* argv[]) {
 
   if ((retval = nc_inq_varid(ncid, "coords", &coords_id)))
     ERR(retval);
+  //TODO: Check that type is correct...
   coords = malloc(sizeof(double)*ncoords*3);
   if ((retval = nc_get_var_double(ncid, coords_id, coords)))
     ERR(retval);
 
+  if ((retval = nc_inq_varid(ncid, "tetrahedron_interior", &tet_int_id)))
+    ERR(retval);
+  //if ((retval = nc_inq_var(ncid, tet_int_id, NULL, &tet_int_type,NULL,NULL,NULL)))
+  //  ERR(retval);
+  //TODO: Check that type is correct...
+  tet_int = malloc(sizeof(int)*tetinterior*tetinteriorsize);
+  if ((retval = nc_get_var_int(ncid, tet_int_id, tet_int)))
+    ERR(retval);
+
+  printf(" Done.\n");
+  
   //Initialize linked lists
   lonely_tris = NULL;
   
@@ -100,21 +143,24 @@ int main(int argc, char* argv[]) {
   f2 = fopen("findBadBC_doubleBC.csv","w");
   if (f2==NULL) { printf("error opening file findBadBC_doubleBC.csv!\n"); exit(1);}
   fprintf(f2, "x coord,y coord,z coord,scalar\n");
-  
-  for (int i = 0; i < tetexterior; i++) {
-    size_t idx_off = i*tetexteriorsize;
-    printf("%i %i %i %i %i %i\n", i, tet_ext[idx_off+1], tet_ext[idx_off+2], tet_ext[idx_off+3], tet_ext[idx_off+4], tet_ext[idx_off+8]);
 
-    checkTri(tet_ext[idx_off+1],tet_ext[idx_off+2],tet_ext[idx_off+3], tet_ext[idx_off+5], coords,i);
-    checkTri(tet_ext[idx_off+1],tet_ext[idx_off+2],tet_ext[idx_off+4], tet_ext[idx_off+7], coords,i);
-    checkTri(tet_ext[idx_off+1],tet_ext[idx_off+3],tet_ext[idx_off+4], tet_ext[idx_off+6], coords,i);
-    checkTri(tet_ext[idx_off+2],tet_ext[idx_off+3],tet_ext[idx_off+4], tet_ext[idx_off+8], coords,i);
+  printf("Looping over exterior tris, calling checkTri() on each.\n This checks if it is seen before (=> it is already in lonely_tris list), if so check that they both are in SS=-1.\n If it matches, delete the old match from lonely_tris.\n It further checks if it matches ANY of the interior tris, if so it should have SS=-1.\n If it is not matching any other tris, then push it to the lonely_tris list.\n");
+  for (int i = 0; i < tetexterior; i++) {
+    if (i%100 == 0) printf("completed=%6.2f%% len(lonely_tris)=%8zu\n", 100*((double)i)/((double)tetexterior),lonely_tris_num);
+    size_t idx_off = i*tetexteriorsize;
+    //printf("%i %i %i %i %i %i\n", i, tet_ext[idx_off+1], tet_ext[idx_off+2], tet_ext[idx_off+3], tet_ext[idx_off+4], tet_ext[idx_off+8]);
+
+    checkTri(tet_ext[idx_off+1],tet_ext[idx_off+2],tet_ext[idx_off+3], tet_ext[idx_off+5], i);
+    checkTri(tet_ext[idx_off+1],tet_ext[idx_off+2],tet_ext[idx_off+4], tet_ext[idx_off+7], i);
+    checkTri(tet_ext[idx_off+1],tet_ext[idx_off+3],tet_ext[idx_off+4], tet_ext[idx_off+6], i);
+    checkTri(tet_ext[idx_off+2],tet_ext[idx_off+3],tet_ext[idx_off+4], tet_ext[idx_off+8], i);
 
   }
   fclose(f2);
+  printf(" Done.\n");
   
   //Find the lonely tris with SS=-1:
-  printf("Writing file for noBC badBCs...\n");
+  printf("Now looking for lonely (surface) tris with SS==-1...\n");
   FILE* f = fopen("findBadBC_noBC.csv","w");
   if (f==NULL) { printf("error opening file findBadBC_noBC.csv!\n"); exit(1);}
   fprintf(f, "x coord,y coord,z coord,scalar\n");
@@ -137,10 +183,15 @@ int main(int argc, char* argv[]) {
     lonely_tris_curr=lonely_tris_curr->next;
   }
   fclose(f);
-
-
+  printf(" Done.\n");
+  
   printf("lonely_noBC=%zu\n",lonely_noBC);
   printf("doubleBC=%zu\n",doubleBC);
+  
+  if (lonely_noBC==0 && doubleBC==0)
+    printf("Mesh seems good :)\n");
+  else
+    printf("Bad mesh -- use Paraview to see where the problems are :(\n");
   
   /* Close the file, freeing all resources. */
   if ((retval = nc_close(ncid)))
@@ -148,14 +199,30 @@ int main(int argc, char* argv[]) {
   
   free(tet_ext);
   tet_ext=NULL;
+
+  free(tet_int);
+  tet_int=NULL;
+
+  free(coords);
+  coords=NULL;
+
+  lonely_tris_curr = lonely_tris;
+  struct tri_listnode* lonely_tris_next = lonely_tris;
+  while (lonely_tris_curr != NULL) {
+    lonely_tris_next = lonely_tris_curr->next;
+    free(lonely_tris_curr);
+    lonely_tris_num--;
+    lonely_tris_curr=lonely_tris_next;
+  }
+  lonely_tris=NULL;
+  lonely_tris_next=NULL;
 }
 
 
-void checkTri(int idx1, int idx2, int idx3, int SS, double* coords, int inTet) {
+void checkTri(int idx1, int idx2, int idx3, int SS, int inTet) {
   // Make sure the indices are in increasing order
   int i1, i2, i3;
-  i1 = idx1; i2=idx2; i3=idx3;
-  
+  i1 = idx1; i2=idx2; i3=idx3;  
   if (!(idx1<idx2 && idx2<idx3)){
     //Mini buble sort
     int itmp;
@@ -164,18 +231,17 @@ void checkTri(int idx1, int idx2, int idx3, int SS, double* coords, int inTet) {
     if (i2>i3) SWAP(i2,i3);
     if (i1>i2) SWAP(i1,i2);
     //if (i2>i3) SWAP(i2,i3);
-
+#undef SWAP
     //printf("%i %i %i\n",i1,i2,i3);
   }
-
-
-  //Debug
+  /*
+  //Debug sorting
   if (!(i1<i2 && i2<i3)){
     printf("ERR\n");
     exit(1);
   }
-
-
+  */
+  
   //Compare the current tri with all known lonely tris:
   struct tri_listnode*  lonely_tris_curr     =  lonely_tris; //List element which we are working on
   struct tri_listnode** lonely_tris_currLink = &lonely_tris; //Pointer to the pointer to the current element (i.e. inside the previous element or the root pointer)
@@ -203,7 +269,14 @@ void checkTri(int idx1, int idx2, int idx3, int SS, double* coords, int inTet) {
     lonely_tris_currLink=&(lonely_tris_curr->next);
     lonely_tris_curr=lonely_tris_curr->next;
   }
-  if (lonely_tris_curr == NULL) { //There was no match. Add a new tri.
+
+  int matchInternal = findInternal(i1,i2,i3);
+  if (matchInternal != -1 && SS != -1) {
+    printf("Bad tri: SS=%i and it matches an internal tri.\n", SS);
+  }
+  
+  if (lonely_tris_curr == NULL && matchInternal==-1) {
+    //There was no match. Add a new tri to the list of lonelies.
     lonely_tris_curr=*lonely_tris_currLink = (struct tri_listnode*) malloc(sizeof(struct tri_listnode));
     lonely_tris_curr->i1=i1;
     lonely_tris_curr->i2=i2;
@@ -215,4 +288,48 @@ void checkTri(int idx1, int idx2, int idx3, int SS, double* coords, int inTet) {
     //printf("%zu\n",lonely_tris_num);
   }
   
+}
+
+int matchInternal(int i1,   int i2,   int i3,
+		  int idx1, int idx2, int idx3) {
+  //Sort idx1,idx2,idx3, then compare it to the (assumed to be sorted)
+  // i1,i2,i3. If they match, return 1, otherwise return 0.
+  
+  // Make sure the indices are in increasing order
+  int j1, j2, j3;
+  j1 = idx1; j2=idx2; j3=idx3;  
+  if (!(j1<j2 && j2<j3)){
+    //Mini buble sort
+    int jtmp;
+#define SWAP(x,y) {jtmp=x;x=y;y=jtmp;}
+    if (j1>j2) SWAP(j1,j2);
+    if (j2>j3) SWAP(j2,j3);
+    if (j1>j2) SWAP(j1,j2);
+#undef SWAP
+  }
+  /*
+  //Debug sorting
+  if (!(j1<j2 && j2<j3)){
+    printf("ERR\n");
+    exit(1);
+  }
+  */
+
+  if (i1==j1 && i2==j2 && i3==j3) {
+    //It's a match with an internal TRI
+    return 1;
+  }
+  return 0;
+}
+int findInternal(int i1, int i2, int i3) {
+  //Return the index of an internal tet containing a tri matching (i1,i2,i3),
+  // otherwise return -1.
+  for(int i = 0; i<tetinterior;i++) {
+    size_t idx_off = i*tetinteriorsize;
+    if (matchInternal(i1,i2,i3, tet_int[idx_off+1],tet_int[idx_off+2],tet_int[idx_off+3])) return i;
+    if (matchInternal(i1,i2,i3, tet_int[idx_off+1],tet_int[idx_off+2],tet_int[idx_off+4])) return i;
+    if (matchInternal(i1,i2,i3, tet_int[idx_off+1],tet_int[idx_off+3],tet_int[idx_off+4])) return i;
+    if (matchInternal(i1,i2,i3, tet_int[idx_off+2],tet_int[idx_off+3],tet_int[idx_off+4])) return i;
+  }
+  return -1; //Nothing found
 }
