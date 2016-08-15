c0687ec227d93d24682edfbdbad2f11717312993
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2016-08-15 17:56:02 +0200

    Fix the transverse wake plotting

--- t3p_ng/plotWake.py
+++ t3p_ng/plotWake.py
@@ -90,8 +90,8 @@ for arg in sys.argv[1:]:
         wx = (Wake(s,V_x,wl.I,0.0,wl.y))
         
         wakes_trans.append((wx,))
-        imps_trans.append(None)
-        envs_trans.append(None)
+        imps_trans.append((ImpedanceSpectrum(wx),))
+        envs_trans.append((Envelope(wx),))
         
     else:
         wakes_trans.append(None)
@@ -104,56 +104,123 @@ if len(wakes) == 0:
 
 for (w,i,e,n, wt,it,et) in zip(wakes,imps,envs,names, wakes_trans,imps_trans,envs_trans):
     print "Plotting '"+n+"'"
+
+    #Longitudinal wake
     plt.figure(1)
     wl = plt.plot(w[0].s, w[0].V/w[0].Q, label=n, ls='-')
     wlc = wl[0].get_color()
     Iscale = max(w[0].V/w[0].Q)/max(w[0].I)
     plt.plot(w[0].s, w[0].I*Iscale, ls='--',color=wlc)
 
+    #Longitudinal wake spectrum (re/im)
     plt.figure(2)
     plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.real(i[0].Z[:i[0].goodIdx]), color=wlc, ls='-', label=n)
     plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.imag(i[0].Z[:i[0].goodIdx]), color=wlc, ls='--')
     
-    
+    #Longitudinal wake spectrum (abs)
     plt.figure(3)
-    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.abs(i[0].Z[:i[0].goodIdx]), label=n)
+    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.abs(i[0].Z[:i[0].goodIdx]), label=n,color=wlc,ls="--")
     #print i[0].f[:i[0].goodIdx]
-    
+
+    #Longitudinal wake envelope
     plt.figure(4)
-    plt.semilogy(e[0].s,e[0].Venv/w[0].Q,label=n)
-    
+    plt.plot(e[0].s,e[0].Venv/w[0].Q,label=n,color=wlc,ls="--")
+
+    #Longitudinal wake (log y)
     plt.figure(5)
-    freqRatioN = i[0].f[:i[0].goodIdx] / 11.2455e3 #f/f_rev
-    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.real(i[0].Z[:i[0].goodIdx]) / freqRatioN, color=wlc, ls='-', label=n)
-    plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.imag(i[0].Z[:i[0].goodIdx] / freqRatioN), color=wlc, ls='--')
+    plt.semilogy(e[0].s,e[0].Venv/w[0].Q,label=n,color=wlc,ls="--")
+    
+    #Normalized spectrum
+    # plt.figure(6)
+    # freqRatioN = i[0].f[:i[0].goodIdx] / 11.2455e3 #f/f_rev
+    # plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.real(i[0].Z[:i[0].goodIdx]) / freqRatioN, color=wlc, ls='-', label=n)
+    # plt.plot(i[0].f[:i[0].goodIdx]/1e9, np.imag(i[0].Z[:i[0].goodIdx] / freqRatioN), color=wlc, ls='--')
 
     if doTrans:
+        #Transverse wake
         plt.figure(11)
-        wx = plt.plot(wt[0].s, wt[0].V/wt[0].Q)
-    
+        plt.plot(wt[0].s, wt[0].V/wt[0].Q,label=n,ls="-")
+
+        #Transverse wake spectrum (re/im)
+        plt.figure(12)
+        plt.plot(it[0].f[:i[0].goodIdx]/1e9, np.real(it[0].Z[:i[0].goodIdx]), color=wlc, ls='-', label=n)
+        plt.plot(it[0].f[:i[0].goodIdx]/1e9, np.imag(it[0].Z[:i[0].goodIdx]), color=wlc, ls='--')
+
+        #Transverse wake spectrum (abs)
+        plt.figure(13)
+        plt.plot(it[0].f[:i[0].goodIdx]/1e9, np.abs(it[0].Z[:i[0].goodIdx]), label=n,color=wlc,ls="--")
+        #print i[0].f[:i[0].goodIdx]
+
+        #Transverse wake envelope
+        plt.figure(14)
+        plt.plot(et[0].s,et[0].Venv/wt[0].Q,label=n,color=wlc,ls="--")
+        
+        #Transverse wake envelope (log y)
+        plt.figure(15)
+        plt.semilogy(et[0].s,et[0].Venv/wt[0].Q,label=n,color=wlc,ls="--")
+        
+        #Normalized transverse spectrum
+        # plt.figure(6)
+        # freqRatioN = it[0].f[:i[0].goodIdx] / 11.2455e3 #f/f_rev
+        # plt.plot(it[0].f[:i[0].goodIdx]/1e9, np.real(it[0].Z[:i[0].goodIdx]) / freqRatioN, color=wlc, ls='-', label=n)
+        # plt.plot(it[0].f[:i[0].goodIdx]/1e9, np.imag(it[0].Z[:i[0].goodIdx] / freqRatioN), color=wlc, ls='--')
+
+        
 plt.figure(1)
 plt.legend()
 plt.xlabel("s [m]")
-plt.ylabel("V [V/pC]")
+plt.ylabel("$V_z$ [V/pC]")
 
 plt.figure(2)
 plt.legend()
 plt.xlabel("f [GHz]")
-plt.ylabel("Z [$\Omega$]")
+plt.ylabel("$Z_z$ [$\Omega$]")
 
 plt.figure(3)
 plt.legend()
 plt.xlabel("f [GHz]")
-plt.ylabel("|Z| [$\Omega$]")
+plt.ylabel("$|Z_z|$ [$\Omega$]")
 
 plt.figure(4)
 plt.legend()
 plt.xlabel("s [m]")
-plt.ylabel("V [V/pC]")
+plt.ylabel("$V_z$ [V/pC]")
 
 plt.figure(5)
 plt.legend()
-plt.xlabel("f [GHz]")
-plt.ylabel("Z [$\Omega$]/($f/f_{rev}$)")
+plt.xlabel("s [m]")
+plt.ylabel("$V_z$ [V/pC]")
+
+
+# plt.figure(6)
+# plt.legend()
+# plt.xlabel("f [GHz]")
+# plt.ylabel("Z [$\Omega$]/($f/f_{rev}$)")
+
+if doTrans:
+    plt.figure(11)
+    plt.legend()
+    plt.xlabel("s [m]")
+    plt.ylabel("$V_x$ [V/pC]")
+    
+    plt.figure(12)
+    plt.legend()
+    plt.xlabel("f [GHz]")
+    plt.ylabel("$Z_x$ [$\Omega$]")
+    
+    plt.figure(13)
+    plt.legend()
+    plt.xlabel("f [GHz]")
+    plt.ylabel("$|Z_x|$ [$\Omega$]")
+    
+    plt.figure(14)
+    plt.legend()
+    plt.xlabel("s [m]")
+    plt.ylabel("$V_x$ [V/pC]")
+
+    plt.figure(15)
+    plt.legend()
+    plt.xlabel("s [m]")
+    plt.ylabel("$V_x$ [V/pC]")
 
 plt.show()
