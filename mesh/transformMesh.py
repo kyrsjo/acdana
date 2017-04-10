d76482df8bbd4629ebd148d49a8c4ad959ae66b4
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2017-04-10 23:16:10 +0200

    Y(z) shifting transformation

--- mesh/transformMesh.py
+++ mesh/transformMesh.py
@@ -36,22 +36,31 @@ for v in mfile_in.variables:
         continue
     mfile_out.variables[v][:,:] = mfile_in.variables[v][:,:]
 
-#Transformation 1: Narrower at the pipe ends
+###########################################################
+#TRANSFORMATION PARAMETERS!!!
+
 ri = 5e-3
-r0 = 110e-3/2.0
+r0 = 101e-3/2.0
 r1 = 111.0e-3/2.0
 
+y_shift = 4.99e-3
 
-ri = 10e-3
-r0 = 50e-3/2.0
-r1 = 111.0e-3/2.0
+#Exaggarated parmeters for demo
+#ri = 10e-3
+#r0 = 50e-3/2.0
+#r1 = 111.0e-3/2.0
+#y_shift = 25e-3
 
-z0_up = -0.32686
-z1_up = -26.86e-3
+assert y_shift <= (r1-r0)
 
-z0_dn = 442.607799e-3
-z1_dn = 142.607799e-3
+z0_up = -326.86e-3    # End of beam pipe
+z1_up =  -26.86e-3    # Transition to central region
 
+z0_dn = 442.607799e-3 # End of beam pipe
+z1_dn = 142.607799e-3 # Transition to central region
+###########################################################
+
+#Transformation 1: Narrower at the pipe ends
 def scaleR(z,r, z0,z1):
     assert r <= r1*(1+1e-10) # We should not be scaling anything outside of this region 
 
@@ -69,6 +78,19 @@ def scaleR(z,r, z0,z1):
     
     S = R/r
     return S
+#Transformation 2: Shift y
+def shiftY(z):
+    if z < z1_up:
+        #return (z-z0_up) * y_shift/(z1_up-z0_up)
+        #Linearly interpolate between (z0_up,0) and (z1_up,y_shift)
+        return (1-(z-z0_up)/(z1_up-z0_up))*0 + (z-z0_up)/(z1_up-z0_up)*y_shift
+    elif z>z1_dn:
+        #return (z-z1_dn) * y_shift/(z1_up-z0_up) - y_shift
+        #Linearly interpolate between (z1_dn,-y_shift) and (z0_dn,0)
+        return (1-(z-z1_dn)/(z0_dn-z1_dn))*(-y_shift) + (z-z1_dn)/(z0_dn-z1_dn)*0
+    else:
+        #Linearly interpolate between (z1_up,y_shift) and (z1_dn,-y_shift)
+        return (1-(z-z1_up)/(z1_dn-z1_up))*y_shift + ((z-z1_up)/(z1_dn-z1_up))*(-y_shift)
 
 #Loop over the coordinates, apply transformation as needed
 ncoords = len(mfile_in.dimensions["ncoords"])
@@ -88,16 +110,19 @@ for i in xrange(ncoords):
     #Z_pre[i] = z
     #R_pre[i] = r
     
-    if z < z1_up:
+    if z < z1_up: #In upstream end
         r = np.sqrt(x**2+y**2)
         S = scaleR(z,r, z0_up,z1_up)
         x *= S
         y *= S
-    elif z > z1_dn:
+    elif z > z1_dn: #In downstream end
         r = np.sqrt(x**2+y**2)
         S = scaleR(z,r, z0_dn,z1_dn)
         x *= S
         y *= S
+    
+    y += shiftY(z)
+        
         
     #r = np.sqrt(x**2+y**2)
     #Z_post[i] = z
